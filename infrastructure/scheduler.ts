ef20204827268ba03a62291d57a089d3989ac4ac
Daria Katysheva <Daria.Katysheva@Student.HTW-Berlin.de>
2023-01-18 15:44:28 +0100

    Fix scheduler

--- infrastructure/scheduler.ts
+++ infrastructure/scheduler.ts
@@ -1,11 +1,12 @@
 import { Cron } from 'https://deno.land/x/croner@5.3.4/src/croner.js';
 import { ProductModel } from '../data/models/ProductModel.ts';
+import { format } from 'https://deno.land/std@0.173.0/datetime/format.ts';
 
 const checkExpirationDates = async () => {
   await ProductModel.updateMany(
-    { expiration_date: new Date() },
+    { expiration_date: format(new Date(), 'dd-MM-yyyy') },
     { isExpired: true }
   );
 };
 
-Cron('@daily', checkExpirationDates);
+const _scheduler = new Cron('@daily', checkExpirationDates);

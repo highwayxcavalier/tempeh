613e2b9301f913a51952cb8615d6f0e5cc8ccdbd
Daria Katysheva <Daria.Katysheva@Student.HTW-Berlin.de>
2023-01-21 23:05:40 +0100

    Add heroku

--- index.ts
+++ index.ts
@@ -4,11 +4,8 @@ import { types } from './schema/schema.ts';
 import { resolvers } from './resolvers/index.ts';
 import mongoose from 'npm:mongoose@6.7';
 import { oakCors } from 'https://deno.land/x/cors/mod.ts';
-import type {
-  APIGatewayProxyEvent,
-  Context,
-} from 'https://deno.land/x/lambda/mod.ts';
 import { handler } from 'https://deno.land/x/serverless_oak/mod.ts';
+import { config } from './config.ts';
 
 const app = new Application();
 
@@ -22,7 +19,9 @@ app.use(oakCors()); // Enable CORS for All Routes
 app.use(GraphQLService.routes(), GraphQLService.allowedMethods());
 
 try {
-  await mongoose.connect('mongodb://127.0.0.1:27017/tofu');
+  await mongoose.connect(
+    config.MONGODB_URL ?? 'mongodb://127.0.0.1:27017/tofu'
+  );
   console.log(`Connected: ${mongoose.connection.readyState}`);
 } catch (error) {
   console.error(error);
@@ -30,11 +29,3 @@ try {
 
 console.log('Server start at http://192.168.0.211:8080');
 await app.listen({ port: 8080 });
-
-export const Echo = async (event: APIGatewayProxyEvent, context: Context) => {
-  return handler(event, context, app);
-};
-
-export default {
-  Echo,
-};
